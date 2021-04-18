b4e041523a543c5990977980cb12ff882c9f85d0
Thorben Below <thorbenbelow@gmail.com>
2021-04-18 10:03:54 +0200

    add init.sh

---
+++ .myconf/init.sh
@@ -0,0 +1,100 @@
+#!/usr/bin/env bash
+
+# update
+echo "Updating system..."
+sudo pacman -Syu
+echo "Finished Updating."
+
+# yay
+if [[ ! -d "$HOME/yay" ]]; then
+	echo "Installing yay..."
+	pacman -S --needed git base-devel
+	git clone https://aur.archlinux.org/yay.git
+	cd yay
+	makepkg -si
+	cd ~
+	echo "yay succesfully installed."
+else
+	echo "yay is already installed."
+fi
+
+# git
+echo "Setting up Git. Please follow the instructions:"
+read -p "Git: Enter name: " name
+read -p "Git: Enter mail: " mail
+git config --global user.name "$name"
+git config --global user.email "$mail"
+git config --global core.editor "nvim"
+git config --global pull.rebase true
+git config --global rebase.autoStash true
+echo "Git configured succesfully."
+
+# Generate ssh-key
+read -p "Create new ssh-key? (Y/N): " confirm
+if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
+	echo "Creating new ssh-key..."
+	ssh-keygen -t ecdsa -b 521 -N '' -f ~/.ssh/id_rsa <<<y 2>&1 >/dev/null
+	xclip -sel c < ~/.ssh/id_ecdsa.pub
+	echo "Public key copied to clipboard."
+else
+	echo "Skipped creating ssh-key."
+fi
+
+# base
+echo "Installing base packages..."
+yay -S --nodiffmenu --noeditmenu --noupgrademenu google-chrome \
+	kitty \
+	neovim \
+	exa \
+	bat \
+	xclip \
+	nerd-fonts-complete
+echo "Installation succesfull."
+
+# snap
+read -p "Install snapd? (Y/N): " confirm
+if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
+	if [[ ! -d "$HOME/snap" ]]; then
+		echo "Installing snapd..."
+		yay -S --nodiffmenu --noeditmenu --noupgrademenu snapd
+		sudo systemctl enable --now snapd.socket
+		sudo ln -s /var/lib/snapd/snap /snap
+		echo "snapd installed succesfully."
+	else
+		echo "snapd is already installed."
+	fi
+else
+	echo "Skipped installing snapd"
+fi
+
+# extended
+read -p "Install extended? (Y/N): " confirm
+if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
+	echo "Installing extended..."
+	yay -S --nodiffmenu --noeditmenu --noupgrademenu --removemake --cleanafter spotify \
+		visual-studio-code-bin \
+		zoom \
+		discord \
+		timeshift \
+		obs-studio
+	sudo snap install intellij-idea-ultimate --classic
+	sudo snap install pycharm-professional --classic
+	sudo snap install datagrip --classic
+	echo "Finished installing extended."
+else
+	echo "Skipped installing extended."
+fi
+
+# docker
+read -p "Install docker? (Y/N): " confirm
+if [[ $confirm == [yY] || $confirm == [yY][eE][sS] ]]; then
+	echo "Installing docker..."
+	yay -S --nodiffmenu --noeditmenu --noupgrademenu docker
+	sudo systemctl start docker.service
+	sudo systemctl enable docker.service
+	echo "Finished installing docker."
+else
+	echo "Skipped installing docker."
+fi
+
+echo "Finished setup."
--- .myconf/readme.md
+++ .myconf/readme.md
@@ -1,77 +1,27 @@
 # Setup guide
 
-## Update
-```Bash
-pacman -Syu
-```
-## yay
-pacman -S --needed git base-devel
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-
-## Base
-```Bash
-yay -S google-chrome \
-	kitty \
-	neovim \
-	exa \
-	bat \
-	xclip \
-	nerd-fonts-complete
-```
-
-## Generate ssh-key
-```Bash
-ssh-keygen -t ecdsa -b 521
-xclip -sel c < ~/.ssh/id_ecdsa.pub
-```
-
-## snapd
-```Bash
-yay -S snapd
-sudo systemctl enable --now snapd.socket
-sudo ln -s /var/lib/snapd/snap /snap
-```
-
-## docker
-```Bash
-yay -S docker
-sudo systemctl start docker.service
-sudo systemctl enable docker.service
-```
-
-## Extended
-```Bash
-yay -S spotify \
-	visual-studio-code-bin \
-	zoom \
-	discord \
-	timeshift
-sudo snap install intellij-idea-ultimate --classic
-sudo snap install pycharm-professional --classic
-sudo snap install datagrip --classic
-```
-
-## git
-```Bash
-git config --global user.name <name>
-git config --global user.email <mail> 
-git config --global core.editor "nvim"
-git config --global pull.rebase true
-git config --global rebase.autoStash true
-```
-
 ## Dotfiles
 ```Bash
 git clone --separate-git-dir=$HOME/.myconf git@gitlab.com:thorbenbelow/dotfiles.git $HOME/myconf-tmp
 rm -r ~/myconf-tmp
 ```
 
-## Windows fonts
+## Setup
 ```Bash
-mkdir /usr/share/fonts/WindowsFonts
-cp /windows/Windows/Fonts/* /usr/share/fonts/WindowsFonts/
-chmod 644 /usr/share/fonts/WindowsFonts/*
+chmod 777 ~/.myconf/init.sh
+~/.myconf/init.sh
+```
+
+## Fonts
+```Bash
+mkdir /usr/share/fonts/<name>
+cp /path/to/fonts/* /usr/share/fonts/<name>/
+chmod 644 /usr/share/fonts/<name>/*
 fc-cache --force
+```
+
+## gdrive
+```Bash
+yay -S drive-bin
+drive init ~/gdrive
 ```
